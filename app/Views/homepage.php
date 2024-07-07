14463202bb605b1cbf07b6c41c4715077579bad8
Dodevca <twelvedotio@gmail.com>
2024-07-07 22:39:56 +0800

    Create jumbotron

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -1,4 +1,52 @@
 <?= $this->extend('layouts/main') ?>
 
 <?= $this->section('main') ?>
+<section id="jumbotron">
+    <div id="jumbotron-carousel" class="carousel slide" data-bs-ride="true">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#jumbotron-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#jumbotron-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#jumbotron-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-ride="true" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)">
+                <div class="container position-relative min-vh-100">
+                    <div class="row position-absolute top-50 start-0 translate-middle-y z-2 w-100">
+                        <div class="col-lg-6 px-3">
+                            <h1 class="text-dark">Example headline.</h1>
+                            <p class="text-muted">Some representative placeholder content for the first slide of the carousel.</p>
+                            <a class="btn btn-secondary rounded-pill" href="#">Action</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-ride="true" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-ride="true" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#jumbotron-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#jumbotron-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
 <?= $this->endSection() ?>
--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -4,14 +4,68 @@
 	<title>Bayuajiprks</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('favicon-16x16.png') ?>">
+    <link rel="manifest" href="<?= base_url('site.webmanifest') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/style.min.css') ?>">
 </head>
 <body>
     <header>
+        <nav class="navbar navbar-expand-lg py-3">
+            <div class="container">
+                <a class="navbar-brand" href="<?= base_url() ?>">
+                    <img src="<?= base_url('images/logo-full.png') ?>" class="logo w-100 h-auto">
+                </a>
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="<?= base_url() ?>">home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">link</a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">link</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="d-flex align-items-center justify-content-end">
+                    <button type="button" class="btn btn-link">
+                        <i class="bi bi-search text-dark"></i>
+                    </button>
+                    <button type="button" class="btn btn-link">
+                        <i class="bi bi-heart text-dark"></i>
+                    </button>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Navbar toggle button">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
     </header>
     <main>
         <?= $this->renderSection('main') ?>
     </main>
     <footer>
     </footer>
+    <script src="<?= base_url('js/bootstrap.bundle.min.js') ?>"></script>
 </body>
 </html>
