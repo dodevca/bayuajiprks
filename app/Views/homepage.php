851237a55b80c95cda504bf5384deb06f18f9d9e
Dodevca <twelvedotio@gmail.com>
2024-07-09 02:01:51 +0800

    Update styling

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -14,7 +14,7 @@
                     <div class="row position-absolute top-50 translate-middle-y z-2 w-100">
                         <div class="col-lg-6 px-3">
                             <span class="badge bg-primary bg-opacity-75 text-white ms-1 mb-3" data-aos="fade-right">Slide 1</span>
-                            <h1 class="h1 text-dark">Example headline</h1>
+                            <h1 class="fs-1">Example headline</h1>
                             <p class="text-muted">Some representative placeholder content for the first slide of the carousel.</p>
                             <a class="btn btn-secondary rounded-pill" href="#" data-aos="fade-up">Action</a>
                         </div>
@@ -26,7 +26,7 @@
                     <div class="row position-absolute top-50 translate-middle-y z-2 w-100">
                         <div class="col-lg-6 px-3">
                             <span class="badge bg-primary bg-opacity-75 text-white ms-1 mb-3" data-aos="fade-right">Slide 2</span>
-                            <h1 class="h1 text-dark">Example headline</h1>
+                            <h2 class="fs-1">Example headline</h2>
                             <p class="text-muted">Some representative placeholder content for the first slide of the carousel.</p>
                         </div>
                     </div>
@@ -37,7 +37,7 @@
                     <div class="row position-absolute top-50 translate-middle-y z-2 w-100" data-aos="fade-right">
                         <div class="col-lg-6 px-3">
                             <span class="badge bg-primary bg-opacity-75 text-white ms-1 mb-3">Slide 3</span>
-                            <h1 class="h1 text-dark">Example headline</h1>
+                            <h2 class="fs-1">Example headline</h2>
                             <p class="text-muted">Some representative placeholder content for the first slide of the carousel.</p>
                         </div>
                     </div>
@@ -51,33 +51,42 @@
         <div class="row">
             <div class="col-sm-6 col-md-3">
                 <div class="category-item p-3 rounded shadow" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)" data-aos="zoom-in-up">
+                    <h3 class="fs-6 text-center mt-2">Lorem</h3>
                     <div class="position-absolute bottom-0 start-50 translate-middle-x">
                         <a href="#" class="btn btn-primary text-white mb-4">Check it out</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-3">
-                <div class="category-item p-3 rounded shadow" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)" data-aos="zoom-in-up" data-aos-delay="300">
+                <div class="category-item p-3 rounded shadow" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)" data-aos="zoom-in-up">
+                    <h3 class="fs-6 text-center mt-2">Ipsum</h3>
                     <div class="position-absolute bottom-0 start-50 translate-middle-x">
                         <a href="#" class="btn btn-primary text-white mb-4">Check it out</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-3">
-                <div class="category-item p-3 rounded shadow" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)" data-aos="zoom-in-up" data-aos-delay="600">
+                <div class="category-item p-3 rounded shadow" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)" data-aos="zoom-in-up">
+                    <h3 class="fs-6 text-center mt-2">Dolor</h3>
                     <div class="position-absolute bottom-0 start-50 translate-middle-x">
                         <a href="#" class="btn btn-primary text-white mb-4">Check it out</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-3">
-                <div class="category-item p-3 rounded shadow" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)" data-aos="zoom-in-up" data-aos-delay="900">
+                <div class="category-item p-3 rounded shadow" style="background-image: url(<?= base_url('images/jumbotron-demo.png') ?>)" data-aos="zoom-in-up">
+                    <h3 class="fs-6 text-center mt-2">Sit Amet</h3>
                     <div class="position-absolute bottom-0 start-50 translate-middle-x">
                         <a href="#" class="btn btn-primary text-white mb-4">Check it out</a>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</section>
+<section id="latest">
+    <div class="container py-5">
+        <h3 class="h2">What's new</h3>
     </div>
 </section>
 <?= $this->endSection() ?>
--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -38,7 +38,7 @@
                     </button>
                 </div>
                 <div class="collapse navbar-collapse order-lg-1" id="navbar-menu">
-                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <ul class="navbar-nav me-auto my-2 my-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="<?= base_url() ?>">home</a>
                         </li>
